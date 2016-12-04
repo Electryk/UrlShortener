5750650ff858447de03afe5a4e09cd8584ca8475
jebiba95 <jesusbintaned95@gmail.com>
2016-12-04 12:31:33 +0100

    Resolución de errores

--- common/src/test/java/urlshortener/common/repository/fixture/LocationFixture.java
+++ common/src/test/java/urlshortener/common/repository/fixture/LocationFixture.java
@@ -1,5 +1,7 @@
 package urlshortener.common.repository.fixture;
 
+import java.sql.Date;
+
 import urlshortener.common.domain.Location;
 import urlshortener.common.domain.ShortURL;
 
@@ -7,11 +9,11 @@ public class LocationFixture {
 
 	public static Location locationSuccess(ShortURL su) {
 		return new Location(su.getHash(), "Zaragoza", "España", "49,4568", "-0,8563", "255.255.255.255",
-				"Aragón", "Universidad de Zaragoza", null);
+				"Aragón", "Universidad de Zaragoza", null, new Date(System.currentTimeMillis()));
 	}
 	
 	public static Location locationFailed(ShortURL su) {
 		return new Location(su.getHash(), null, "private range", null, null, "255.255.255.255",
-				null, null, null);
+				null, null, null, new Date(System.currentTimeMillis()));
 	}
 }
