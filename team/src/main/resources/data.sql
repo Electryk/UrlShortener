299fe7a86bf21e8b5b5cc6f7af424295654a22ce
Jesus Bintaned <jesusbintaned95@gmail.com>
2017-01-15 22:49:17 +0100

    Resolucionde errores

--- team/src/main/resources/data.sql
+++ team/src/main/resources/data.sql
@@ -1,4 +1,4 @@
 
-insert into shorturl (hash, target, sponsor, created, owner, mode, safe, ip, country, clicks) values ('e4f06474', 'http://google.es', '', CURRENT_TIMESTAMP, '', 0, true, '', '', 0);
-insert into location (id, hash, city, country, ip, lat, lng, region, created, organization) values (1, 'e4f06474', 'Zaragoza', 'Spain', '155.210.211.35', 41.6453, -0.8849, 'Aragon', CURRENT_TIMESTAMP, 'Universidad de Zaragoza');
-insert into location (id, hash, city, country, ip, lat, lng, region, created, organization) values (2, 'e4f06474', 'Zaragoza', 'Spain', '155.210.211.35', 41.6453, -0.8849, 'Aragon', CURRENT_TIMESTAMP, 'Universidad de Zaragoza')
+insert into shorturl (hash, target, sponsor, created, owner, mode, safe, ip, country, clicks) values ('e4f06484', 'http://google.es', '', CURRENT_TIMESTAMP, '', 0, true, '', '', 0);
+insert into location (id, hash, city, country, ip, lat, lng, region, created, organization) values (1, 'e4f06484', 'Zaragoza', 'Spain', '155.210.211.35', 41.6453, -0.8849, 'Aragon', CURRENT_TIMESTAMP, 'Universidad de Zaragoza');
+insert into location (id, hash, city, country, ip, lat, lng, region, created, organization) values (2, 'e4f06484', 'Zaragoza', 'Spain', '155.210.211.35', 41.6453, -0.8849, 'Aragon', CURRENT_TIMESTAMP, 'Universidad de Zaragoza')
